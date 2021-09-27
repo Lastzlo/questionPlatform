3e82fef4bbc2d5fb9f8c11bcfe20cacfc5786e94
Lastzlo <z.last.zlo@gmail.com>
2021-09-27 12:00:33 +0300

    feat add 'id' getter setter

--- src/main/java/com/example/questionPlatform/models/QuestionDTO.java
+++ src/main/java/com/example/questionPlatform/models/QuestionDTO.java
@@ -14,6 +14,14 @@ public class QuestionDTO {
     private long FormId;
 
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public long getFormId() {
         return FormId;
     }

82dd9131a417fb7f65c5fb0aa705cd9af12cf2bf
Lastzlo <z.last.zlo@gmail.com>
2021-09-27 16:49:30 +0300

    feat add AnswerController.java

---
+++ src/main/java/com/example/questionPlatform/controllers/AnswerController.java
@@ -0,0 +1,88 @@
+package com.example.questionPlatform.controllers;
+
+import com.example.questionPlatform.models.AnswerDTO;
+import com.example.questionPlatform.models.UserDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("answers")
+public class AnswerController {
+
+    @Operation(summary = "Returns all answers by question id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "302", description = "All answers successfully found",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = AnswerDTO.class)))}),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Answers not found",
+                    content = @Content)
+    })
+    @GetMapping("questions/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<AnswerDTO> getAllUsers(@PathVariable long id) {
+        return List.of();
+    }
+
+    @Operation(summary = "Add answer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "New answer created successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = AnswerDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Incorrect answer's value",
+                    content = @Content)
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void addAnswer(@Valid @RequestBody AnswerDTO answerDTO){
+        //Some realization
+    }
+
+    @Operation(summary = "Update answer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Answer successfully updated",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = AnswerDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Incorrect answer's value",
+                    content = @Content),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Answer not found",
+                    content = @Content)
+    })
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("{id}")
+    public void updateAnswer(@PathVariable long id, @Valid @RequestBody AnswerDTO answerDTO){
+        //Some realization
+    }
+
+    @Operation(summary = "Delete answer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Answer successfully deleted",
+                    content = @Content),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Answer not found",
+                    content = @Content)
+    })
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("{id}")
+    public void deleteAnswer(@PathVariable long id){
+        //Some realization
+    }
+
+
+}

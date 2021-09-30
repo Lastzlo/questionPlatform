7b77b699527a020862da267a38e40f7be4718e60
Maxde1 <61392177+Maxde1@users.noreply.github.com>
2021-09-30 15:40:13 +0300

    Update ResultController.java

--- src/main/java/com/example/questionPlatform/controllers/ResultController.java
+++ src/main/java/com/example/questionPlatform/controllers/ResultController.java
@@ -16,38 +16,7 @@ import javax.validation.Valid;
 @RequestMapping("results")
 public class ResultController {
 
-//    @Operation(summary = "Create new result")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "201", description = "The result was successfully added",
-//                    content = {@Content(
-//                            mediaType = "application/json",
-//                            schema = @Schema(implementation = ResultDTO.class))}),
-//            @ApiResponse(
-//                    responseCode = "400",
-//                    description = "Result is not valid",
-//                    content = @Content)
-//    })
-//    @PostMapping()
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public void createResult(@Valid @RequestBody ResultDTO resultDTO) {
-//        //adding result
-//    }
-//
-//
-//    @Operation(summary = "Delete chosen result")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "200",
-//                    description = "The result was successfully deleted",
-//                    content = @Content),
-//            @ApiResponse(responseCode = "404",
-//                    description = "Result not found",
-//                    content = @Content)
-//    })
-//    @DeleteMapping("{id}")
-//    @ResponseStatus(HttpStatus.OK)
-//    public void deleteResult(@PathVariable long id) {
-//        //deleting result
-//    }
+
     @Operation(summary = "Get all user's result")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
